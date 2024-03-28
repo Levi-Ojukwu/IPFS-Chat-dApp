997bcd8aee4a6177da666529b6beb013800ab547
Levi-Ojukwu <ojukwulevichinedu@gmail.com>
2024-03-28 12:14:32 +0100

    Still in Progress

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,11 @@
+// import { Flex } from "@radix-ui/themes";
+
+export default function Header() {
+  return (
+    <div className="flex justify-between items-center mx-4 mt-4">
+      
+      <div className="text-5xl text-orange-400 font-semibold italic"><span className="text-blue-400">Chat</span>DApp</div>
+        <w3m-button />
+    </div>
+  );
+}
